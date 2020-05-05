2c6e71e7976997cc98b47d676ed581b06b26dd4b
Ulises <UlisesEmiliano.Sosa@stud-inf.unibz.it>
2020-05-05 22:53:57 +0200

    methods of Loader and Retriever  classes with a more defined scope

--- src/main/java/com/OpenDataHub/requests/Requests.java
+++ src/main/java/com/OpenDataHub/requests/Requests.java
@@ -1,7 +1,11 @@
 package com.OpenDataHub.requests;
 
+import java.io.IOException;
+
 public class Requests {
-  public static void main(String[] args) {
-    ;
+  public static void main(String[] args) throws IOException, InterruptedException {
+    
+    Retriever retrieve = new Retriever();
+    retrieve.checkAndRequest();
   }
 }
