90adfb1411747681f167224c98106d87d7cb9dda
Gabriel.Schwienbacher <gabriel.schwienbacher@stud-inf.unibz.it>
2020-06-04 09:33:42 +0200

    Added JsonFile class and FileParser

---
+++ src/main/java/com/OpenDataHub/json/JsonFile.java
@@ -0,0 +1,65 @@
+package com.OpenDataHub.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonFile extends File {
+
+    private String json;
+
+    private JsonNode root;
+
+
+    public JsonFile(String pathname, String jsonData) throws JsonProcessingException {
+        super(pathname);
+        json = jsonData;
+        CreateRootFromString();
+    }
+
+    public JsonFile(String pathname, JsonNode node) {
+        super(pathname);
+        root = node;
+    }
+
+
+
+    public void Save() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(this, root);
+    }
+
+
+    public static JsonFile Open(String path) throws IOException {
+        ObjectMapper omapper = new ObjectMapper();
+        byte[] data = Files.readAllBytes(Paths.get(path));
+        JsonNode jnode = omapper.readTree(data);
+        return new JsonFile(path,jnode);
+    }
+
+
+    private void CreateRootFromString() throws JsonProcessingException {
+        if(json == null) {
+            root = new ObjectMapper().readTree(json);
+        }
+    }
+
+
+    public JsonNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(JsonNode root) {
+        this.root = root;
+    }
+
+
+}
