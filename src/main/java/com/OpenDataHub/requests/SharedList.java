d59bad2c66adff5912e094056602c4eb1bc592f1
riccardo rigoni <riccardo.rigoni@stud-inf.unibz.it>
2020-06-19 14:22:08 +0200

    SharedList object

---
+++ src/main/java/com/OpenDataHub/requests/SharedList.java
@@ -0,0 +1,65 @@
+/**
+ * The class contains a list of FutureTasks for storing the responses retrieved from the Api
+ * 
+ * @author Sosa Ulises
+ */
+package com.OpenDataHub.requests;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class SharedList {
+
+  public static List<FutureTask<StringBuffer>> responsesList = new LinkedList<>();
+
+  private static boolean finished = false;
+  
+  /***
+   * Add a new list and set the number of iterations
+   * @param newResponsesList new list for getting "done" responses
+   */
+  public static void addResponsesList(List<FutureTask<StringBuffer>> newResponsesList) {
+    responsesList = newResponsesList;
+    // int sizeList = newResponsesList.size();
+    // setNumberOfIterations(sizeList);
+  }
+
+  /**
+   * retrieve a String from the FutureTasks list. The method iterate continuosly through the list, until it 
+   * founds a "done" object
+   * 
+   * @return return String response; "-1" if no more elements in the list to wait for 
+   * @throws InterruptedException
+   * @throws ExecutionException
+   */
+  public static String getNewElement() throws InterruptedException, ExecutionException {
+    String newElement = "";
+
+    //sinchronized for thread save access
+    synchronized (responsesList) {
+      //iterate from inital element of the list
+      int index = 0;
+      
+      while (!finished) {
+        if (responsesList.size() == 0) 
+          finished = true;
+        
+        else if (responsesList.get(index).isDone()) {
+      
+        newElement = responsesList.remove(index).get().toString();
+        // logger.debug("element number" + i);
+
+        return newElement;
+        } 
+        else
+          index = (index + 1) % (responsesList.size());
+      
+      }
+
+      //if responsesList is empty -> no need to wait more for new elements to be done
+      return "-1";
+    }
+  }
+}
